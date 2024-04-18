33f8ce549811cd4c1346971f9c53ea748d8f13f8
Jethro Block <jethroblock22@gmail.com>
2024-04-18 12:54:39 +0100

    Add useEffect hook to force iframe reload when CALENDLY_LINK changes

--- src/components/CalendlyFormComponent.js
+++ src/components/CalendlyFormComponent.js
@@ -1,33 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { InlineWidget } from 'react-calendly';
 
 export const CalendlyFormComponent = ({ CALENDLY_LINK }) => {
+  const [key, setKey] = useState(0);
+
+  // Whenever CALENDLY_LINK changes, update the key to force a re-render of the iframe
+  useEffect(() => {
+    setKey(prevKey => prevKey + 1);
+  }, [CALENDLY_LINK]);
+
   return (
     <div className="p-4">
-    <div className="overflow-y-auto shadow-lg rounded-lg h-[75vh] min-w-[30vw]"> 
-      <iframe 
-        title="Webinar Sign Up Form"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true" 
-        width="100%" 
-        height="100%" 
-        frameborder="0" 
-        marginheight="0" 
-        marginwidth="0"
-      >
-        Loading…
-      </iframe>
+      <div className="overflow-y-auto shadow-lg rounded-lg h-[75vh] min-w-[30vw]"> 
+        <iframe 
+          key={key} // Add key prop to force iframe reload when CALENDLY_LINK changes
+          title="Webinar Sign Up Form"
+          src={CALENDLY_LINK} 
+          width="100%" 
+          height="100%" 
+
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
-  </div>
-      );
-    };
-    
-    // <InlineWidget
-    //     styles={{
-    //       height: '100%', 
-    //       width: '100%', 
-    //     }}
-    //     url={CALENDLY_LINK}
-    //     prefill={{}}
-    //     pageSettings={{}}
-    //     utm={{}}
-    //   />
+  );
+};
