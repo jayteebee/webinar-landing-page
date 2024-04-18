3c009ee3946b2feb08d3288db13ea625e8ff28ac
Jethro Block <jethroblock22@gmail.com>
2024-04-18 13:45:12 +0100

    Update Calendly links in content.js

--- src/content/content.js
+++ src/content/content.js
@@ -23,7 +23,7 @@ export const CONTENT = [
         HOST_NAME: "Andrew Milner",
         HOST_TITLE: "Director",
         WEBINAR_DESCRIPTION: "Dive into our upcoming webinar focused on the revolutionary application of FLIR thermal imaging cameras in PC Circuit Board Imaging (EDT). As a distinguished distributor of FLIR products, we're excited to guide you through the cutting-edge process of Enhanced Diagnostics through Thermal imaging. This session aims to enlighten participants on how thermal imaging technology is pivotal in identifying hot spots, component failures, and various potential issues in circuit boards before they lead to critical failures. Ideal for engineers, technicians, and quality control professionals in the electronics manufacturing industry, this webinar will cover the basics of thermal imaging, its integration into PC circuit board diagnostics, and how it can dramatically improve product reliability and performance. Discover the indispensable tool that thermal imaging has become in the electronics industry and how it can be applied to enhance your diagnostics and maintenance strategies.",
-        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSffaXTRGOolU-WbTqrNRPFM-ODJ3sexgJNQaUELDrRSo75Jfw/viewform?embedded=true",
+        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true",
         WEBINAR_IMAGE: { src: thermalCarHS, alt: "image of a thermal car" },
 
     },
@@ -34,7 +34,7 @@ export const CONTENT = [
         HOST_NAME: "Matthew Clavey",
         HOST_TITLE: "Director",
         WEBINAR_DESCRIPTION: "Gear up for an exhilarating webinar where we spotlight the High Speed Imaging capabilities of the FLIR X Series HS cameras, complemented by their integration with visible spectrum imaging. As a premier distributor of FLIR thermal imaging solutions, we're thrilled to present an in-depth look into how these high-speed cameras are revolutionizing industries by capturing thermal events at incredibly high frame rates. Perfect for R&D professionals, engineers, and scientists working in sectors where dynamic thermal events are critical, this session will explore the X Series HS's unparalleled ability to visualize and analyze the thermal properties of objects in motion, in real-time. Learn how combining high-speed thermal imaging with visible light capture can unveil insights into heat generation, dissipation, and transfer processes that were previously invisible. Join us to see how FLIR's high-speed imaging technology is opening new avenues in research, development, and testing across various applications.",
-        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSeXq48YM3IPSe6bWjDLaiE9MZTp7xIG51SN5evcDE2e3Etjsg/viewform?embedded=true",
+        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true",
         WEBINAR_IMAGE: { src: thermalCarHS, alt: "image of a thermal car" },
     },
     {
@@ -44,7 +44,7 @@ export const CONTENT = [
         HOST_NAME: "Matthew Clavey",
         HOST_TITLE: "Director",
         WEBINAR_DESCRIPTION: "Prepare to embark on a journey through the expansive capabilities of FLIR thermal imaging cameras for Outdoor Imaging over Long Distances. As an authorized distributor, we are excited to present a webinar that showcases the prowess of FLIR cameras in monitoring, security, and surveillance applications across vast expanses. This session is specifically designed for professionals in security, wildlife observation, and any field requiring the monitoring of large outdoor areas. We will cover the technological innovations that enable FLIR cameras to detect and identify objects from great distances, ensuring unparalleled clarity and precision. Learn about the different models suited for long-range imaging, their specific features, and how they can be integrated into your existing systems to enhance your operational efficiency. Whether you're safeguarding a perimeter or tracking wildlife, discover how our thermal imaging solutions can transform your long-distance observation and analysis capabilities.",
-        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSf5B-E2JBU2VW0DbKoLeNiakyqpqZQGFSMu6y9qhUDM9VzGIQ/viewform?embedded=true",
+        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true",
         WEBINAR_IMAGE: { src: thermalCarHS, alt: "image of a thermal car" },
 
     },
@@ -55,7 +55,7 @@ export const CONTENT = [
         HOST_NAME: "Matthew Clavey",
         HOST_TITLE: "Director",
         WEBINAR_DESCRIPTION: "Unlock the full potential of your FLIR thermal imaging cameras with our upcoming webinar on the Hidden Gems of thermal imaging technology, including Filters, Superframing, and the Blackbody Reference (BB Reference) technique. Tailored for thermal imaging enthusiasts and professionals alike, this session is designed to shed light on the lesser-known features and functionalities that can significantly enhance your thermal imaging experience. Discover how to utilize Filters to improve image quality and focus on specific temperature ranges, explore Superframing to capture high-contrast scenes, and understand the importance of the Blackbody Reference for absolute temperature measurement accuracy. These advanced techniques open up new possibilities for precision and efficiency in a wide array of applications, from scientific research to industrial diagnostics. Whether you're looking to refine your thermal imaging skills or explore new ways to apply your FLIR camera,",
-        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSd-0RzaKEyCo3xmzCkIez6wpW26QJxv19_mj3IpDpRvsP5aWw/viewform?embedded=true",
+        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true",
         WEBINAR_IMAGE: { src: thermalCarHS, alt: "image of a thermal car" },
 
     },
@@ -66,7 +66,7 @@ export const CONTENT = [
         HOST_NAME: "Andrew Milner",
         HOST_TITLE: "Director",
         WEBINAR_DESCRIPTION: "Join us for an enlightening webinar on the critical roles of Sync and Trigger functionalities in maximizing the efficiency and effectiveness of FLIR thermal imaging cameras across various use cases. This session, brought to you by a trusted distributor of FLIR products, is tailor-made for professionals who seek to harness the full capabilities of their thermal imaging equipment in synchronized multi-camera setups or require precise triggering for capturing thermal events. We will delve into real-world applications demonstrating how synchronization can coordinate multiple cameras for comprehensive area coverage or detailed analysis of complex phenomena, and how triggering mechanisms can be optimized for capturing transient thermal events at just the right moment. Ideal for sectors including advanced manufacturing, R&D, and automation, this webinar will provide valuable insights into enhancing your thermal imaging operations through effective use of Sync and Trigger functionalities.",
-        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSeRVLSQ2cQi3u7iDykrCOoGt6jLRJEQ6MJN4r90zhjRmVD-gA/viewform?embedded=true",
+        CALENDLY_LINK: "https://docs.google.com/forms/d/e/1FAIpQLSe5tImbf5MYxJUmATYHOzgaVrirWAnF9-pqxkVXaR50WxH_Xg/viewform?embedded=true",
         WEBINAR_IMAGE: { src: thermalCarHS, alt: "image of a thermal car" },
 
     },
