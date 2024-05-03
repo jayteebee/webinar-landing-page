18dbfe591242112ad6f232a363381f88697008d2
Jethro Block <jethroblock22@gmail.com>
2024-05-03 15:14:28 +0100

    Update webinar title in content.js

--- src/content/content.js
+++ src/content/content.js
@@ -7,7 +7,7 @@ import logo from "../images/ThermalVision_logo.png"
 export const CONTENT = [
     {
         id: "Cooled vs Uncooled",
-        WEBINAR_TITLE: "Introduction to Thermal Imaging in R&D and Science.",
+        WEBINAR_TITLE: "Introduction to Thermal Imaging in R&D and Science",
         HOST_IMAGE: { src: andy, alt: "andy" },
         HOST_NAME: "Andrew Milner",
         HOST_TITLE: "Technical Sales Director",
