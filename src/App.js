cc7430814da51308ed2f2129850c2d33466c7556
Jethro Block <jethroblock22@gmail.com>
2024-05-07 12:19:45 +0100

    Refactor homepage layout and component styles

--- src/App.js
+++ src/App.js
@@ -65,6 +65,34 @@ import { CONTENT } from "./content/content.js";
 
 import { IntroSelection } from "./components/IntroSelection.js";
 
+// const HomePage = () => {
+//   const [webinar, setWebinar] = useState("");
+//   const content = CONTENT.find((item) => item.id === webinar) || CONTENT[0];
+
+//   return (
+//     <div className="relative min-h-screen min-w-full">
+//       <div className="relative z-10">
+//         <HeaderComponent />
+//         <IntroSelection setWebinar={setWebinar} />
+//         <div className="flex space-around flex-col lg:flex-row ">
+//           <ContentComponent
+//             id={content.id}
+//             WEBINAR_DESCRIPTION={content.WEBINAR_DESCRIPTION}
+//             WEBINAR_TITLE={content.WEBINAR_TITLE}
+//             HOST_IMAGE={content.HOST_IMAGE}
+//             HOST_NAME={content.HOST_NAME}
+//             HOST_TITLE={content.HOST_TITLE}
+//           />
+//           <CalendlyFormComponent CALENDLY_LINK={content.CALENDLY_LINK} />
+//           {/**
+//           <ImageContentComponent WEBINAR_IMAGE={content.WEBINAR_IMAGE} />
+//          */}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const HomePage = () => {
   const [webinar, setWebinar] = useState("");
   const content = CONTENT.find((item) => item.id === webinar) || CONTENT[0];
@@ -74,24 +102,31 @@ const HomePage = () => {
       <div className="relative z-10">
         <HeaderComponent />
         <IntroSelection setWebinar={setWebinar} />
-        <div className="flex space-around flex-col lg:flex-row ">
-          <ContentComponent
-            id={content.id}
-            WEBINAR_DESCRIPTION={content.WEBINAR_DESCRIPTION}
-            WEBINAR_TITLE={content.WEBINAR_TITLE}
-            HOST_IMAGE={content.HOST_IMAGE}
-            HOST_NAME={content.HOST_NAME}
-            HOST_TITLE={content.HOST_TITLE}
-          />
-          <CalendlyFormComponent CALENDLY_LINK={content.CALENDLY_LINK} />
-          {/**
-          <ImageContentComponent WEBINAR_IMAGE={content.WEBINAR_IMAGE} />
-         */}
+        <div className="flex flex-col lg:flex-row justify-center items-stretch mx-auto px-4 lg:px-8">
+          <div className="flex flex-1 justify-center">
+            <ContentComponent
+              id={content.id}
+              WEBINAR_DESCRIPTION={content.WEBINAR_DESCRIPTION}
+              WEBINAR_TITLE={content.WEBINAR_TITLE}
+              HOST_IMAGE={content.HOST_IMAGE}
+              HOST_NAME={content.HOST_NAME}
+              HOST_TITLE={content.HOST_TITLE}
+              style={{ flex: "1 0 50%", maxWidth: "50%" }}
+            />
+          </div>
+          <div className="flex flex-1 justify-center mt-4 lg:mt-0">
+            <CalendlyFormComponent
+              CALENDLY_LINK={content.CALENDLY_LINK}
+              style={{ flex: "1 0 50%", maxWidth: "50%" }}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+
 
 const App = () => {
   return (
